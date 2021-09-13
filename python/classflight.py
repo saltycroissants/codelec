d4e1000be3af1bde61a83347dd1dfa2a118640c5
saltycroissants <eunjilee55@gmail.com>
2021-09-13 18:53:31 +0900

    codes  added until oop

---
+++ python/classflight.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+class Flight():
+    def __init__(self, total_passengers):
+        self.total_passengers = total_passengers
+        self.passengers = []
+    
+    def add_passenger(self, name):
+        if self.open_seats() == 0 :
+            return False
+        
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.total_passengers - len(self.passengers)
+
+flightA = Flight(3)
+
+persons = ["Harry", "Ron", "Hermione", "Neville"]
+for person in persons:
+    success = flightA.add_passenger(person)
+    if success:
+        print(f"added {person} to flight successfully")
+    else :
+        print(f"no seat for {person}")
---
+++ python/usesquare.py
@@ -0,0 +1,4 @@
+from functions import square
+
+for i in range(10):
+    print(f"Inside usesquare: The square of {i} is {square(i)}")
